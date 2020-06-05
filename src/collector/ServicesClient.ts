bcc0a655899446d875316931fa353e529ed9deff
Charlie Duong <charlieduong94@gmail.com>
2020-06-05 16:18:06 -0400

    Don't promisfy meraki controller functions because they already return promises

--- src/collector/ServicesClient.ts
+++ src/collector/ServicesClient.ts
@@ -1,5 +1,4 @@
 import meraki = require('meraki');
-import { promisfy } from 'promisfy';
 import {
   MerakiOrganization,
   MerakiNetwork,
@@ -29,10 +28,7 @@ export class ServicesClient {
    * Get Organizations
    */
   async getOrganizations(): Promise<MerakiOrganization[]> {
-    const getOrganizations = promisfy(
-      meraki.OrganizationsController.getOrganizations,
-    );
-    const res: object[] = await getOrganizations();
+    const res: object[] = await meraki.OrganizationsController.getOrganizations();
     return res[0] as MerakiOrganization[];
   }
 
@@ -40,10 +36,9 @@ export class ServicesClient {
    * Get Admin Users of an Organization
    */
   async getAdmins(organizationId: string): Promise<MerakiAdminUser[]> {
-    const getOrganizationAdmins = promisfy(
-      meraki.AdminsController.getOrganizationAdmins,
+    const res: object[] = await meraki.AdminsController.getOrganizationAdmins(
+      organizationId,
     );
-    const res: object[] = await getOrganizationAdmins(organizationId);
     return res[0] as MerakiAdminUser[];
   }
 
@@ -51,10 +46,9 @@ export class ServicesClient {
    * Get SAML Roles of an Organization
    */
   async getSamlRoles(organizationId: string): Promise<MerakiSamlRole[]> {
-    const getOrganizationSamlRoles = promisfy(
-      meraki.SAMLRolesController.getOrganizationSamlRoles,
+    const res: object[] = await meraki.SAMLRolesController.getOrganizationSamlRoles(
+      organizationId,
     );
-    const res: object[] = await getOrganizationSamlRoles(organizationId);
     return res[0] as MerakiSamlRole[];
   }
 
@@ -62,12 +56,11 @@ export class ServicesClient {
    * Get Networks of an Organization
    */
   async getNetworks(organizationId: string): Promise<MerakiNetwork[]> {
-    const getOrganizationNetworks = promisfy(
-      meraki.NetworksController.getOrganizationNetworks,
+    const res: object[] = await meraki.NetworksController.getOrganizationNetworks(
+      {
+        organizationId,
+      },
     );
-    const res: object[] = await getOrganizationNetworks({
-      organizationId,
-    });
     return res[0] as MerakiNetwork[];
   }
 
@@ -75,8 +68,9 @@ export class ServicesClient {
    * Get VLANs in a Network
    */
   async getVlans(networkId: string): Promise<MerakiVlan[]> {
-    const getNetworkVlans = promisfy(meraki.VlansController.getNetwork_vlans);
-    const res: object[] = await getNetworkVlans(networkId);
+    const res: object[] = await meraki.VlansController.getNetwork_vlans(
+      networkId,
+    );
     return res[0] as MerakiVlan[];
   }
 
@@ -84,10 +78,9 @@ export class ServicesClient {
    * Get Devices in a Network
    */
   async getDevices(networkId: string): Promise<MerakiDevice[]> {
-    const getNetworkDevices = promisfy(
-      meraki.DevicesController.getNetworkDevices,
+    const res: object[] = await meraki.DevicesController.getNetworkDevices(
+      networkId,
     );
-    const res: object[] = await getNetworkDevices(networkId);
     return res[0] as MerakiDevice[];
   }
 
@@ -95,10 +88,9 @@ export class ServicesClient {
    * Get Clients in a Network
    */
   async getClients(networkId: string): Promise<MerakiClient[]> {
-    const getNetworkClients = promisfy(
-      meraki.ClientsController.getNetworkClients,
-    );
-    const res: object[] = await getNetworkClients({ networkId });
+    const res: object[] = await meraki.ClientsController.getNetworkClients({
+      networkId,
+    });
     return res[0] as MerakiClient[];
   }
 
@@ -106,8 +98,9 @@ export class ServicesClient {
    * Get SSIDs of a Wireless Network
    */
   async getSSIDs(networkId: string): Promise<MerakiSSID[]> {
-    const getNetworkSSIDs = promisfy(meraki.SsidsController.getNetwork_ssids);
-    const res: object[] = await getNetworkSSIDs(networkId);
+    const res: object[] = await meraki.SsidsController.getNetwork_ssids(
+      networkId,
+    );
     return res[0] as MerakiSSID[];
   }
 }
