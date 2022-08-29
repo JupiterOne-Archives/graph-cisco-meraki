5687c49076552af31ba1d37b1746deaa5cedcfab
Matthew Zember <matthew.zember@jupiterone.com>
2022-08-29 16:32:46 -0400

    set key for network has client relationship

--- src/converter/relationships.ts
+++ src/converter/relationships.ts
@@ -9,19 +9,28 @@ import {
 } from '@jupiterone/integration-sdk-core';
 import { MappedRelationships, TargetEntities } from '../constants';
 
+function createNetworkVlanHasClientKey(sourceKey: string, targetId: string) {
+  return (
+    sourceKey +
+    RelationshipClass.HAS +
+    TargetEntities.CLIENT._type +
+    ':' +
+    targetId
+  );
+}
+
 export const convertNetworkClientRelationship = (
   sourceEntity: Entity,
   client: MerakiClient,
 ): Relationship => {
   return createMappedRelationship({
     _class: RelationshipClass.HAS,
+    _key: createNetworkVlanHasClientKey(sourceEntity._key, client.id),
     _type: MappedRelationships.NETWORK_HAS_CLIENT._type,
     source: sourceEntity,
     targetFilterKeys: [['_class', 'macAddress']],
     target: {
       ...convertProperties(client),
-      _key: TargetEntities.CLIENT._type + ':' + client.id,
-      _type: TargetEntities.CLIENT._type,
       _class: TargetEntities.CLIENT._class,
       displayName: client.description || client.mac,
       macAddress: client.mac,
@@ -39,6 +48,7 @@ export const convertVlanClientRelationship = (
   return createMappedRelationship({
     _class: RelationshipClass.HAS,
     _type: MappedRelationships.VLAN_HAS_CLIENT._type,
+    _key: createNetworkVlanHasClientKey(sourceEntity._key, client.id),
     source: sourceEntity,
     targetFilterKeys: [['_class', 'macAddress']],
     target: {
